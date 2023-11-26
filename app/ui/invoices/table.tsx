2f060992d1a977bc648fd9c6aceae336912c75e1
allen <alejandro0209one@gmail.com>
2023-11-26 15:53:34 -0700

    Error image corregido

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -31,7 +31,7 @@ export default async function InvoicesTable({
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt={`${invoice.name}'s profile picture`}
+                        alt={`${invoice.name}  profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -86,6 +86,7 @@ export default async function InvoicesTable({
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
                       <Image
+                      alt='dcdf'
                         src={invoice.image_url}
                         className="rounded-full"
                         width={28}
